97a742ea6f001f01dd2396dd0a4645ca7f552b6d
Aniket Yadav <anikety7905@gmail.com>
2025-09-20 23:51:09 +0530

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -1,1 +1,9 @@
 
+import random
+from Config import loan_data
+
+def get_random_applicant():
+    """
+    Selects a random applicant's data from the loan_data list.
+    """
+    return random.choice(loan_data)
